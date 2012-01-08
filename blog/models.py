44cff6c44ed0f37eca457710b93d676241c9b310
Erik Rose <erik@mozilla.com>
2012-01-07 17:33:51 -0800

    Create Article model.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db.models import Model, DateTimeField, TextField, DateField
+
+
+class Article(Model):
+    # In Postgres, "text" is identical to "varchar" without length limits, and
+    # specifying limits only decreases performance. it has no effect on how
+    # it's stored. Not giving a max length here saves a pricy migration,
+    # inevitably, we want to increase it.
+    title = TextField()  # TODO: index?
+    slug = TextField(db_index=True)
+    created = DateTimeField(auto_now_add=True, db_index=True)  # index for sort
+    body = TextField()
